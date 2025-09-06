be4571273b3f8c598917f4364fe1b0649553c16d
dpatil1719 <patildhanashree888@gmail.com>
2025-09-05 17:26:52 -0700

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,40 +1,49 @@
 // passport.js
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const passportJWT = require('passport-jwt');
-const { User } = require('./models');
+const { Strategy: LocalStrategy } = require('passport-local');
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt');
+const { User } = require('./models.js');
 
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJwt = passportJWT.ExtractJwt;
-
+// Local (username/password) login
 passport.use(
   new LocalStrategy(
     { usernameField: 'Username', passwordField: 'Password' },
     async (username, password, done) => {
       try {
         const user = await User.findOne({ Username: username });
-        if (!user) return done(null, false, { message: 'Incorrect username or password.' });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username or password.' });
+        }
         if (!user.validatePassword(password)) {
           return done(null, false, { message: 'Incorrect username or password.' });
         }
         return done(null, user);
-      } catch (e) {
-        return done(e);
+      } catch (err) {
+        return done(err);
       }
     }
   )
 );
 
+// JWT auth for protected routes
 passport.use(
   new JWTStrategy(
-    { jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: process.env.JWT_SECRET || 'dev_only_secret' },
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'dev_only_secret',
+      algorithms: ['HS256'],
+      ignoreExpiration: false
+    },
     async (payload, done) => {
       try {
         const user = await User.findById(payload._id);
-        return done(null, user || false);
-      } catch (e) {
-        return done(e, false);
+        if (!user) return done(null, false);
+        return done(null, user);
+      } catch (err) {
+        return done(err, false);
       }
     }
   )
 );
+
+module.exports = passport;
